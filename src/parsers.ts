47b59f760d59948da3abe394975ddc6f79936920
Raniere Gaia Costa da Silva <Raniere.CostadaSilva@gesis.org>
2026-06-17 15:33:08 +0200

    Add context menu for Quarto
    Related to https://github.com/notebook-link/jupyter-plainb/issues/14

--- src/parsers.ts
+++ src/parsers.ts
@@ -10,11 +10,16 @@ import {
 } from 'plainb';
 import type { Notebook } from 'plainb';
 
+// parseQuartoMd should be implemented in plainb
+// https://github.com/notebook-link/plainb/issues/18
+var parseQuartoMd = parseClassicMd;
+
 export type ParserName =
   | 'parsePy'
   | 'parseSphinxGallery'
   | 'parseClassicMd'
-  | 'parseMystMd';
+  | 'parseMystMd'
+  | 'parseQuartoMd';
 
 export interface IRule {
   dir: string;
@@ -38,33 +43,38 @@ export const PARSERS: Record<ParserName, (text: string) => object> = {
   parsePy,
   parseSphinxGallery,
   parseClassicMd,
-  parseMystMd
+  parseMystMd,
+  parseQuartoMd
 };
 
 export const SERIALIZERS: Record<ParserName, (notebook: Notebook) => string> = {
   parsePy: toPy,
   parseSphinxGallery: toSphinxGallery,
   parseClassicMd: toClassicMd,
-  parseMystMd: toMystMd
+  parseMystMd: toMystMd,
+  parseQuartoMd: toClassicMd
 };
 
 export const PARSER_LABELS: Record<ParserName, string> = {
   parsePy: 'Percent format (.py)',
   parseSphinxGallery: 'Sphinx Gallery (.py)',
   parseClassicMd: 'Classic Markdown (.md)',
-  parseMystMd: 'MyST Notebook (.md)'
+  parseMystMd: 'MyST Notebook (.md)',
+  parseQuartoMd: 'Quarto (.qmd)'
 };
 
 export const PARSER_EXTENSIONS: Record<ParserName, string[]> = {
   parsePy: ['.py'],
   parseSphinxGallery: ['.py'],
   parseClassicMd: ['.md'],
-  parseMystMd: ['.md']
+  parseMystMd: ['.md'],
+  parseQuartoMd: ['.qmd']
 };
 
 export const CONTEXT_MENU_LABELS: Record<ParserName, string> = {
   parsePy: 'Notebook (Percent .py)',
   parseSphinxGallery: 'Notebook (Sphinx Gallery .py)',
   parseClassicMd: 'Notebook (Classic Markdown .md)',
-  parseMystMd: 'Notebook (MyST .md)'
+  parseMystMd: 'Notebook (MyST .md)',
+  parseQuartoMd: "Notebook (Quarto .qmd)"
 };
